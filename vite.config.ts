751eb666eedd678f284d26a0340a41bda36f2131
Rajiv Kumar Yadav <rajiv0099c@gmail.com>
2023-12-10 11:45:32 +0530

    added env secret in deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, BuildOptions } from "vite"
+
 
 export default defineConfig({
+  // build: {
+  //   envDir: process.cwd(),
+  // },
   plugins: [react()],
   resolve: {
     alias: {
