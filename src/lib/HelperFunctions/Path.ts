11fd2aeb50e4c5bc94239c01e56d58ab49bb08c5
Rajiv Kumar Yadav <rajiv0099c@gmail.com>
2023-12-15 12:16:34 +0530

    FW - Added route paths as variables

---
+++ src/lib/HelperFunctions/Path.ts
@@ -0,0 +1,16 @@
+const base = "/Sindalah";
+const main = "/Sindalah";
+
+const signin = `${base}/sign-in`; 
+const signup = `${base}/sign-up`; 
+const verification = `${base}/verification`; 
+    const verification_email = `${base}/verification/email`; 
+        const verification_email_sent = `${base}/verification/email/sent`; 
+        const verification_email_fail = `${base}/verification/email/fail`; 
+        const verification_email_done = `${base}/verification/email/done`; 
+    const verification_verifyemail = `${base}/verification/verify_email`; 
+    const verification_phone = `${base}/verification/phone`; 
+
+export const paths = {base,main,signin,signup,verification,verification_email,verification_email_sent,verification_email_fail,verification_email_done,verification_verifyemail,verification_phone};
+
+
