509fed0f422a5c32e1bd5c7c31b70cf9a91acaeb
Rajiv Kumar Yadav <rkcuwork@gmail.com>
2023-12-28 12:00:52 +0530

    Modified RootLayout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -5,11 +5,7 @@ import Loader from "@/components/Shared/Loader";
 const RootLayout = () => {
   return (
     <div className="flex-center gap-2">
-      <Spinner/>
-      <Loader/>
-      
-
-
+      <h1>Welcome to the App. Login Successfull.</h1>
     </div>
   )
 }
